a7aa6d4cbc6c040defc517cd6f286548d45dd8e1
RaistlinD2x-Dev <83308842+RaistlinD2x-Dev@users.noreply.github.com>
2022-09-11 17:42:31 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('My New App!\n')
+   response.end('MY CLOUD FORMATION TEMPLATE!\n')
 }).listen(3333)
 
 // Console will print the message
